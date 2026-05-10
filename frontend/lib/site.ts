af87c4505dc0bf6e652bd5673585b77dd73ff088
vishal <vishalsinghvicky95@gmail.com>
2026-05-10 14:18:06 +0530

    update address

--- frontend/lib/site.ts
+++ frontend/lib/site.ts
@@ -9,7 +9,7 @@ export const CONTACT_EMAIL = 'info@kirtibuildwell.com'
 export const CONTACT_PHONE_DISPLAY = '+91 9565077981'
 export const CONTACT_PHONE_LINK = 'tel:+919565077981'
 export const CONTACT_ADDRESS_LINES = [
-  'The Mica Complex, Nursery Phulwari Road',
+  'The Meera Complex, Nursery Phulwari Road',
   'Madharmau, Lucknow',
   'Uttar Pradesh 226002',
   'India'
